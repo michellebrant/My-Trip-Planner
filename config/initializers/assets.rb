9c4b87f33b18495aaa510c26ec3d1616b670ceab
Michelle Brant <michellebrant>
2016-12-29 19:12:35 -0500

    added expandable trips

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,10 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( script.js )
 Rails.application.config.assets.precompile += %w( availableCities.js )
+Rails.application.config.assets.precompile += %w( yourtrips.css )
+Rails.application.config.assets.precompile += %w( trips.css )
+Rails.application.config.assets.precompile += %w( bsscript.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
